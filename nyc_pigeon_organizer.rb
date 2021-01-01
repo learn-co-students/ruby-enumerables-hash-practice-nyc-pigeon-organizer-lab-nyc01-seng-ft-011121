90f20d02e884ec0a6783c4bdbbaa5ceb26774651
Dayne Dewar <daynedewar@yahoo.com>
2021-01-01 20:59:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,18 @@
+#key_1 is color, gender, lives
+#value_1 are the colors, genders and locations
+require 'pry'
 def nyc_pigeon_organizer(data)
-  hash = Hash.new
-  data.each do |key_1, value_1|      #key_1 is color, gender, lives
-    value_1.find do |value_2|        #value_1 are the colors, genders
-    if value_2 == "Theo"                #and locations
-     hash << value_2
-   end
- end
- end
+new_hash = data.each_with_object({}) do |(k, v), array|
+    v.each do |inner_k, names|
+      names.each do |name|
+        if !array[name]
+          array[name] = {}
+        end
+        if !array[name][k]
+          array[name][k]=[]
+        end
+        array[name][k] << inner_k.to_s
+      end  
+    end
+  end
 end
