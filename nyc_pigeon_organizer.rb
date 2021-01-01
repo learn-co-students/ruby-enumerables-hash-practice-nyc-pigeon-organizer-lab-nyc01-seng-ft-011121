d905109cd1ab01fbb215fd01ea9db63cde1cc9cd
Dayne Dewar <daynedewar@yahoo.com>
2021-01-01 18:22:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = Hash.new
+  data.each do |key_1, value_1|      #key_1 is color, gender, lives
+    value_1.find do |value_2|        #value_1 are the colors, genders
+    if value_2 == "Theo"                #and locations
+     hash << value_2
+   end
+ end
+ end
 end
