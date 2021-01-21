218ea76d8e958b453f63d6cde528d5cf283a804c
Jamie Pham <jpham1109@gmail.com>
2021-01-20 22:44:10 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  list = data.each_with_object({}) do |(k, v) list|
+  list = data.each_with_object({}) do |(k, v), list|
     v.each do |inner_k, names|
       names.each do |name|
         if !list[name]
           list[name] = {}
         end
         if !list[name][k]
-          list[name][k] = {}
-        list[name][k].push(inner_k.to_s)
+          list[name][k] = []
         end
+        list[name][k].push(inner_k.to_s)
       end 
     end 
   end 
